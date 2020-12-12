8a5b3b2582331a5c643d452e36f34d63fd72a6f4
xcoders <57432765+xcoders-hub@users.noreply.github.com>
2020-12-12 17:03:41 +0530

    Update generate_session_string.py

--- app/generate_session_string.py
+++ app/generate_session_string.py
@@ -9,8 +9,8 @@ except:
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 
-api_id = os.environ['API_ID']#int(input("Enter your API_ID: "))
-api_hash = os.environ['API_HASH']#input("Enter your API_HASH: ")
+api_id = 1064864
+api_hash = "5f3eeab0e6108731551e6a93598b654c"
 
 with TelegramClient(StringSession(), api_id, api_hash) as client:
     print(client.session.save())
